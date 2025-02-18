d40c547fb3683cb2584077ea8d728bcf799702c5
Ali Shoja <alishojaa88@gmail.com>
2025-02-18 18:09:47 -0400

    chacked mongodb version

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { connectToDatabase } from "../../../../../lib/mongodb";
+import { connectToDatabase } from "../../../../../lib/mongodb"; 
 import bcrypt from "bcrypt";
 
 const handler = NextAuth({
